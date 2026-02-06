3ffd62d62cea3226186351911d4892e497ed15c4
Abdelazim Ramadan <Abdelazim Ramadan>
2026-02-06 16:34:55 +0200

    feat: create Message entity

---
+++ lib/features/chat/domain/entities/message.dart
@@ -0,0 +1,28 @@
+import 'package:equatable/equatable.dart';
+
+class Message extends Equatable {
+  final String content;
+  final MessageSender sender;
+
+  const Message({required this.content, required this.sender});
+
+  bool get isUser => sender == MessageSender.user;
+  bool get isAI => sender == MessageSender.ai;
+
+  Message copyWith({String? content, MessageSender? sender}) {
+    return Message(
+      content: content ?? this.content,
+      sender: sender ?? this.sender,
+    );
+  }
+
+  @override
+  List<Object?> get props => [content, sender];
+
+  @override
+  String toString() {
+    return 'Message(content: $content, sender: $sender)';
+  }
+}
+
+enum MessageSender { user, ai }
